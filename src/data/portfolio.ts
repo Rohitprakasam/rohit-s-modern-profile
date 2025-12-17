1cfc790611ca06ac0855431f3024a35c933dd3a2
Rajalakshmi G <rajalakshmig1981@gmail.com>
2025-12-17 16:25:51 +0530

    chore(cms): update portfolio data via admin panel

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -7,7 +7,7 @@ export const portfolioData = {
     },
     "heroSection": {
         "greeting": "Hi I am",
-        "fullName": "Rohit PRAKASAM g",
+        "fullName": "Rohit_rp",
         "designation": "System Administrator",
         "tagline": "Cybersecurity & Linux Infrastructure Specialist",
         "ctaButtons": [
